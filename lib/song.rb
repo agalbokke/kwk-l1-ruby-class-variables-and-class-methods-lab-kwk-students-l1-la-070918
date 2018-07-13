33c63539c488dec9716852b02809cbfb0b56a27b
Ashyah Hewage <ashyahgalbokke@gmail.com>
2018-07-13 18:26:00 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,92 @@
 
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    @@genre_1 = {}
+    @@genres.each do |genre|
+      @@genre_1[genre] ||= 0
+      @@genre_1[genre] += 1
+    end
+    @@genre_1
+  end
+
+  def self.artist_count
+    @@artist_1 = {}
+    @@artists.each do |artist|
+      @@artist_1[artist] ||= 0
+      @@artist_1[artist] += 1
+    end
+    @@artist_1
+  end
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres.push self.genre
+    @@artists.push self.artist
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Song
+#   attr_accessor :name
+#   attr_accessor :artist
+#   attr_accessor :genre
+
+# @@song_count => 0
+
+# def initialize(song)
+#   @@count += 1
+# end
+
+# def genre 
+#   @@genre
+  
+# end
+
+# def artists
+#   @@artists
+  
+# end
+
+
+
+
+
+
+
+# end
